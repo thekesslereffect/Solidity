e56236e8fb435b265639a9cf86cd238e92972f9a
thekesslereffect <eldron2323@gmail.com>
2023-04-08 21:12:38 -0700

    updated giveaway based on chatGPT feedback

--- TrashPandazClub/Giveaways/TPCGiveaway.sol
+++ TrashPandazClub/Giveaways/TPCGiveaway.sol
@@ -2,24 +2,25 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TPCGiveaway {
     using Counters for Counters.Counter;
     Counters.Counter private giveawayIds;
-    using SafeERC20 for ERC20;
-    address public erc20Token;
+    using SafeERC20 for IERC20;
+    IERC20 public erc20Token;
     mapping(address => bool) admin;
     mapping(address => bool) public blacklist;
     mapping(address => bool) public whitelist;
     bool public whitelistEnabled;
     uint public creatorFee;
-    uint public creatorRoyalty;
+    uint public creatorRoyaltyPercent;
     bool public entryFeeEnabled;
     bool public creatorFeeEnabled;
+    mapping(address=>uint) public creatorRoyalties;
 
-    struct Giveaway{
+    struct Giveaway {
         address creator;
         uint entryFee;
         string prize;
@@ -27,19 +28,16 @@ contract TPCGiveaway {
         uint startTime;
         uint duration;
         address[] participants;
-        mapping(address => bool) entered;
-        mapping(address => bool) won;
+        mapping(address => uint) enteredIndex;
         address[] winners;
-        bool ended;
-        uint royaltyPool;
     }
     mapping(uint => Giveaway) public giveaways;
 
-    constructor(address _erc20Token){
+    constructor(IERC20 _erc20Token){
         erc20Token = _erc20Token;
         admin[msg.sender] = true;
         creatorFee = 1000; // erc20*10**18
-        creatorRoyalty = 80; // %
+        creatorRoyaltyPercent = 80; // %
         creatorFeeEnabled = false;
         entryFeeEnabled = false;
     }
@@ -63,21 +61,39 @@ contract TPCGiveaway {
         _;
     }
 
+    modifier onlyCreator(uint _giveawayId){
+        require(giveaways[_giveawayId].creator == msg.sender, "Only the giveaway creator can call this function.");
+        _;
+    }
+
     // Events ...........................................
 
     event WinnersPicked(uint giveawayId, address[] winners);
     event GiveawayCreated(uint giveawayId,address creator,uint entryFee, string prize, uint numberOfWinners,uint startTime, uint duration);
     event NewParticipant(uint giveawayaId, address participant);
-    event RoyaltyTransferred(uint giveawayaId, address creator, uint royalty);
+    event RoyaltyWithdrawn(address indexed creator, uint256 amount);
+    event GiveawayDestroyed(uint giveawayId);
 
     // Functions ........................................
 
+    // Creates a new giveaway with the provided parameters and stores it in the giveaways mapping
+    // Deducts the creator fee from the creator's royalties if they have enough, otherwise transfers it from the creator's balance
+    // Emits a GiveawayCreated event
     function createGiveaway(uint _entryFee, string memory _prize, uint _numberOfWinners, uint _duration) public blacklisted whitelisted {
-        uint feeToPay = creatorFee * 10**18;
         if(creatorFeeEnabled) {
-            require(ERC20(erc20Token).allowance(msg.sender, address(this)) >= feeToPay, "Insufficient allowance");
-            require(ERC20(erc20Token).balanceOf(msg.sender) >= feeToPay, "You don't have enough tokens to create a giveaway.");
-            ERC20(erc20Token).safeTransferFrom(msg.sender, address(this), feeToPay);
+            uint feeToPay = creatorFee * 10**18;
+            uint royalties = creatorRoyalties[msg.sender];
+            if (royalties >= feeToPay) {
+                royalties -= feeToPay;
+                creatorRoyalties[msg.sender] = royalties;
+            }else{
+                require(
+                    IERC20(erc20Token).allowance(msg.sender, address(this)) >= feeToPay &&
+                    IERC20(erc20Token).balanceOf(msg.sender) >= feeToPay,
+                    "Insufficient balance or allowance to enter."
+                );
+                IERC20(erc20Token).safeTransferFrom(msg.sender, address(this), feeToPay);
+            }
         }
         giveawayIds.increment();
         uint _giveawayId = giveawayIds.current();
@@ -101,56 +117,56 @@ contract TPCGiveaway {
         );
     }
 
+    // Allows a user to enter a giveaway by providing its ID
+    // Transfers the required entry fee from the participant's balance and updates the creator's royalties
+    // Emits a NewParticipant event
     function enterGiveaway(uint _giveawayId) public blacklisted{
-        // make sure they have enough tokens to enter
-        if(giveaways[_giveawayId].entryFee>0){
-            require(ERC20(erc20Token).allowance(msg.sender, address(this)) >= (giveaways[_giveawayId].entryFee*10**18), "Insufficient allowance");
-            require(ERC20(erc20Token).balanceOf(msg.sender) >= (giveaways[_giveawayId].entryFee*10**18), "You dont have enough tokens to enter.");
-            ERC20(erc20Token).safeTransferFrom(msg.sender, address(this), (giveaways[_giveawayId].entryFee*10**18));
-            giveaways[_giveawayId].royaltyPool += (giveaways[_giveawayId].entryFee*10**18);
-        }
         require(giveaways[_giveawayId].creator != msg.sender, "You cant enter your own giveaway.");
-        require(giveaways[_giveawayId].entered[msg.sender] == false,"You already entered this giveaway.");
+        require(giveaways[_giveawayId].enteredIndex[msg.sender] == 0, "You already entered this giveaway.");
         require(giveaways[_giveawayId].startTime+giveaways[_giveawayId].duration >= block.timestamp, "This giveaway has ended. Sorry.");
-        require(giveaways[_giveawayId].ended == false, "This giveaway has ended. Sorry.");
+        require(giveaways[_giveawayId].winners.length == 0, "This giveaway has ended. Sorry.");
+        if(giveaways[_giveawayId].entryFee>0){
+            require(
+                IERC20(erc20Token).allowance(msg.sender, address(this)) >= giveaways[_giveawayId].entryFee*10**18 &&
+                IERC20(erc20Token).balanceOf(msg.sender) >= giveaways[_giveawayId].entryFee*10**18,
+                "Insufficient balance or allowance to enter."
+            );
+            IERC20(erc20Token).safeTransferFrom(msg.sender, address(this), giveaways[_giveawayId].entryFee*10**18);
+            creatorRoyalties[giveaways[_giveawayId].creator] += giveaways[_giveawayId].entryFee *10**18 * creatorRoyaltyPercent / 100;
+        }
+        giveaways[_giveawayId].enteredIndex[msg.sender] = giveaways[_giveawayId].participants.length;
         giveaways[_giveawayId].participants.push(msg.sender);
-        giveaways[_giveawayId].entered[msg.sender] = true;
         emit NewParticipant(_giveawayId,msg.sender);
     }
 
-    function endGiveaway(uint _giveawayId, uint _seed) public blacklisted whitelisted{
-        if(admin[msg.sender]==false){
-            require(giveaways[_giveawayId].creator == msg.sender, "Only the giveaway creator can pick winners!");
-            require(giveaways[_giveawayId].participants.length > 0, "There were no participants. Sorry.");
-            require(giveaways[_giveawayId].startTime + giveaways[_giveawayId].duration <= block.timestamp, "This giveaway is still running.");
-        }
-        require(giveaways[_giveawayId].ended == false, "This giveaway already ended");
-
-        // In case number of winners allowed exceedsd the participants
+    // Ends a giveaway by picking winners and updating the winners array in the giveaway struct
+    // Emits a WinnersPicked event
+    function endGiveaway(uint _giveawayId, uint _seed) public blacklisted whitelisted onlyCreator(_giveawayId){
+        require(giveaways[_giveawayId].startTime + giveaways[_giveawayId].duration <= block.timestamp, "This giveaway is still running.");
+        require(giveaways[_giveawayId].participants.length > 0, "There were no participants. Sorry.");
+        require(giveaways[_giveawayId].winners.length == 0, "This giveaway has ended. Sorry.");
         if(giveaways[_giveawayId].numberOfWinners>=giveaways[_giveawayId].participants.length){
             giveaways[_giveawayId].winners = giveaways[_giveawayId].participants;
             emit WinnersPicked(_giveawayId, giveaways[_giveawayId].winners);
         } else{
             address[] memory _winners = new address[](giveaways[_giveawayId].numberOfWinners);
-            uint256[] memory _usedIndexes = new uint256[](giveaways[_giveawayId].numberOfWinners);
-
-            for (uint256 i = 0; i < giveaways[_giveawayId].numberOfWinners; i++) {
-                uint256 _randomIndex = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, i, _seed))) % giveaways[_giveawayId].participants.length;
-                while (isIndexUsed(_usedIndexes, _randomIndex)) {
+            uint[] memory _usedIndexes = new uint[](giveaways[_giveawayId].numberOfWinners);
+            for (uint i = 0; i < giveaways[_giveawayId].numberOfWinners; i++) {
+                uint _randomIndex = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, i, _seed))) % giveaways[_giveawayId].participants.length;
+                while (indexIsUsed(_usedIndexes, _randomIndex)) {
                     _randomIndex = (_randomIndex + 1) % giveaways[_giveawayId].participants.length;
                 }
                 _usedIndexes[i] = _randomIndex;
                 _winners[i] = giveaways[_giveawayId].participants[_randomIndex];
             }
             giveaways[_giveawayId].winners = _winners;
-            emit WinnersPicked(_giveawayId, giveaways[_giveawayId].winners);
+            emit WinnersPicked(_giveawayId, _winners);
         }
-        transferRoyalties(_giveawayId);
-        giveaways[_giveawayId].ended = true;
     }
 
-    function isIndexUsed(uint256[] memory usedIndexes, uint256 randomIndex) internal pure returns (bool) {
-        for (uint256 i = 0; i < usedIndexes.length; i++) {
+    // Checks if a given index is present in the usedIndexes array
+    function indexIsUsed(uint[] memory usedIndexes, uint randomIndex) internal pure returns (bool) {
+        for (uint i = 0; i < usedIndexes.length; i++) {
             if (usedIndexes[i] == randomIndex) {
                 return true;
             }
@@ -158,16 +174,19 @@ contract TPCGiveaway {
         return false;
     }
 
-    function transferRoyalties(uint _giveawayId) private {
-        uint _royalty = giveaways[_giveawayId].royaltyPool * creatorRoyalty / 100;
-        if(_royalty > 0){
-            ERC20(erc20Token).safeTransferFrom(address(this),giveaways[_giveawayId].creator, _royalty);
-            emit RoyaltyTransferred(_giveawayId,giveaways[_giveawayId].creator,(_royalty/10**18));
-        }
+    // Allows a creator to withdraw their earned royalties
+    // Emits a RoyaltyWithdrawn event
+    function withdrawRoyalties() public {
+        uint royalties = creatorRoyalties[msg.sender];
+        require(royalties > 0, "No royalties to withdraw.");
+        IERC20(erc20Token).safeTransfer(msg.sender, royalties);
+        creatorRoyalties[msg.sender] = 0;
+        emit RoyaltyWithdrawn(msg.sender, royalties);
     }
 
     // View ........................................................
 
+    // Returns the time remaining for a giveaway to end
     function viewTimeUntilEnd(uint _giveawayId) public view returns(uint){
         uint _time = giveaways[_giveawayId].startTime + giveaways[_giveawayId].duration - block.timestamp;
         if(_time>0){
@@ -177,18 +196,21 @@ contract TPCGiveaway {
         }
     }
 
+    // Returns the winners of a giveaway
     function viewWinners(uint _giveawayId) public view returns(address[] memory){
         address[] memory _winners = new address[](giveaways[_giveawayId].winners.length);
         _winners = giveaways[_giveawayId].winners;
         return _winners;
     }
 
+    // Returns the participants of a giveaway
     function viewParticipants(uint _giveawayId) public view returns(address[] memory){
         address[] memory _participants = new address[](giveaways[_giveawayId].participants.length);
         _participants = giveaways[_giveawayId].participants;
         return _participants;
     }
 
+    // Returns the number of participants in a giveaway
     function viewNumberOfParticipants(uint _giveawayId) public view returns(uint){
         uint _participants = giveaways[_giveawayId].participants.length;
         return _participants;
@@ -196,55 +218,72 @@ contract TPCGiveaway {
 
     // ADMIN ........................................................
 
-    function destroyGiveaway(uint _giveawayId, bool _bool) public adminRole{
-        giveaways[_giveawayId].ended = _bool;
+    // Allows an admin to destroy a giveaway by setting its duration to 0 and winners to an empty array
+    // Emits a GiveawayDestroyed event
+    function destroyGiveaways(uint[] memory _giveawayId) public adminRole{
+        for(uint i=0;i<_giveawayId.length;i++){
+            giveaways[_giveawayId[i]].duration = 0;
+            address[] memory emptyWinners;
+            giveaways[_giveawayId[i]].winners = emptyWinners;
+            emit GiveawayDestroyed(_giveawayId[i]);
+        }
     }
 
+    // Allows an admin to update the creator fee
     function setCreatorFee(uint _creatorFee) public adminRole{
         creatorFee = _creatorFee;
     }
 
-    function setCreatorRoyalty(uint _creatorRoyalty) public adminRole{
-        creatorRoyalty = _creatorRoyalty;
+    // Allows an admin to update the creator royalty percentage
+    function setCreatorRoyaltyPercent(uint _creatorRoyaltyPercent) public adminRole{
+        creatorRoyaltyPercent = _creatorRoyaltyPercent;
     }
 
-    // Set + Remove token
-    function setERC20Token(address _erc20Token) public adminRole{
+    // Allows an admin to set a new ERC20 token as the currency for giveaways
+    function setERC20Token(IERC20 _erc20Token) public adminRole{
         erc20Token = _erc20Token;
     }
     
-    function removeTokens(address _recipient, uint256 _tokenAmount) public adminRole{
-        ERC20(erc20Token).safeTransfer(_recipient, _tokenAmount);
+    // Allows an admin to remove tokens from the contract
+    function removeTokens(address _recipient,IERC20 _token, uint _tokenAmount) public adminRole{
+        IERC20(_token).safeTransfer(_recipient, _tokenAmount);
     }
 
+    // Allows an admin to remove Matic/Polygon from the contract
     function removeMatic(address payable _recipient) public adminRole{
         _recipient.transfer(address(this).balance);
     }
 
+    // Allows an admin to add or remove an admin
     function addAdmin(address _address, bool _bool) public adminRole{
         admin[_address] = _bool;
     }
 
+    // Allows an admin to add or remove addresses from the blacklist
     function addBlacklist(address[] memory _address, bool _bool) public adminRole{
         for(uint i=0;i<_address.length;i++){
             blacklist[_address[i]] = _bool;
         }
     }
 
+    // Allows an admin to add or remove addresses from the whitelist
     function addWhitelist(address[] memory _address, bool _bool) public adminRole{
         for(uint i=0;i<_address.length;i++){
             whitelist[_address[i]] = _bool;
         }
     }
 
+    // Allows an admin to enable or disable the whitelist feature
     function setWhitelistEnabled(bool _bool) public adminRole{
         whitelistEnabled = _bool;
     }
 
+    // Allows an admin to enable or disable the creatorFee feature
     function setCreatorFeeEnabled(bool _bool) public adminRole{
         creatorFeeEnabled = _bool;
     }
 
+    // Allows an admin to enable or disable the entryFee feature
     function setEntryFeeEnabled(bool _bool) public adminRole{
         entryFeeEnabled = _bool;
     }
